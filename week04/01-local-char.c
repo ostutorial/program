7b755b964529c5d393e0a1b1cb979008e7790fab
Intan Dwi Hapsari <intandwihaphapsari@gmail.com>
2016-09-29 17:43:40 +0700

    h

---
+++ week04/01-local-char.c
@@ -0,0 +1,32 @@
+/*
+ * (c) 2016 Rahmat M. Samik-Ibrahim -- This is free software
+ * REV01 Thu Sep 22 10:29:16 WIB 2016
+ * REV00 Thu Sep 22 10:29:16 WIB 2016
+ * START Tue Sep 20 17:00:52 WIB 2016
+ */
+
+#include <stdio.h>
+
+
+void main() {
+   /* Local Variables */
+   char   varchr0='a';
+   char   varchr1='b';
+   char   varchr2='c';
+   char   varchr3='d';
+   char   varchr4='e';
+   char   varchr5='f';
+   char   varchr6='g';
+   char   varchr7='h';
+   printf("=============================================\n");
+   printf("varchr0: value = %2c, address = %p\n", varchr0, &varchr0);
+   printf("varchr1: value = %2c, address = %p\n", varchr1, &varchr1);
+   printf("varchr2: value = %2c, address = %p\n", varchr2, &varchr2);
+   printf("varchr3: value = %2c, address = %p\n", varchr3, &varchr3);
+   printf("varchr4: value = %2c, address = %p\n", varchr4, &varchr4);
+   printf("varchr5: value = %2c, address = %p\n", varchr5, &varchr5);
+   printf("varchr6: value = %2c, address = %p\n", varchr6, &varchr6);
+   printf("varchr7: value = %2c, address = %p\n", varchr7, &varchr7);
+   printf("=============================================\n");
+}
+
